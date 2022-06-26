ec46b37b4d762ab8476aa3244d274e3e4313eb1a
zhangyimi <369217411@qq.com>
2022-06-27 01:14:41 +0800

    modify the input parameters of the example level interpret function
    Change-Id: I9861aafc74a73687ac919d3d02d19c018926a4e8

--- trustai/interpretation/example_level/method/feature_similarity.py
+++ trustai/interpretation/example_level/method/feature_similarity.py
@@ -38,13 +38,14 @@ class FeatureSimilarityModel(ExampleBaseInterpreter):
         ExampleBaseInterpreter.__init__(self, paddle_model, device, predict_fn, classifier_layer_name)
         self.paddle_model = paddle_model
         self.classifier_layer_name = classifier_layer_name
-        self.train_feature, _ = self.extract_featue(paddle_model, train_dataloader)
+        self.train_feature, _ = self.extract_feature_from_dataloader(train_dataloader)
 
+    
     def interpret(self, data, sample_num=3, sim_fn="cos"):
         """
         Select most similar and dissimilar examples for a given data using the `sim_fn` metric.
         Args:
-            data(iterable): Dataloader to interpret.
+            data(iterable): one batch of data to interpret.
             sample_num(int: default=3): the number of positive examples and negtive examples selected for each instance. Return all the training examples ordered by `influence score` if this parameter is -1.
             sim_fn(str: default=cos): the similarity metric to select examples. It should be ``cos``, ``dot`` or ``euc``.
         """
@@ -52,7 +53,7 @@ class FeatureSimilarityModel(ExampleBaseInterpreter):
             sample_num = len(self.train_feature)
         pos_examples = []
         neg_examples = []
-        val_feature, preds = self.extract_featue(self.paddle_model, data)
+        val_feature, preds = self.extract_feature(self.paddle_model, data)
         if sim_fn == "dot":
             similarity_fn = dot_similarity
         elif sim_fn == "cos":
@@ -61,7 +62,7 @@ class FeatureSimilarityModel(ExampleBaseInterpreter):
             similarity_fn = euc_similarity
         else:
             raise ValueError(f"sim_fn only support ['dot', 'cos', 'euc'] in feature similarity, but gets `{sim_fn}`")
-        for index, target_class in enumerate(preds):
+        for index in range(len(preds)):
             tmp = similarity_fn(self.train_feature, paddle.to_tensor(val_feature[index]))
             pos_idx, neg_idx = get_top_and_bottom_n_examples(tmp, sample_num=sample_num)
             pos_examples.append(pos_idx)
@@ -71,16 +72,24 @@ class FeatureSimilarityModel(ExampleBaseInterpreter):
         return res
 
     @paddle.no_grad()
-    def extract_featue(self, paddle_model, data_loader):
+    def extract_feature(self, paddle_model, data):
         """
-        extract feature for data_loader.
+        extract feature for batch data.
         """
-        paddle_model.eval()
-        print("Extracting feature for given dataloader, it will take some time...")
-        features, preds = [], []
+        if self.paddle_model.training:
+            self.paddle_model.eval()
+        feature, _, pred = self.predict_fn(data)
+        return paddle.to_tensor(feature), paddle.to_tensor(pred)
 
-        for step, batch in enumerate(data_loader, start=1):
-            feature, _, pred = self.predict_fn(batch)
-            features.extend(feature)
-            preds.extend(pred)
-        return paddle.to_tensor(features), paddle.to_tensor(preds)
+    def extract_feature_from_dataloader(self, dataloader):
+        """
+        extract feature from data_loader.
+        """
+        print("Extracting feature from given dataloader, it will take some time...")
+        features, preds = [], []
+        
+        for batch in dataloader:
+            feature, pred = self.extract_feature(self.paddle_model, batch)
+            features.append(feature)
+            preds.append(pred)
+        return paddle.concat(features, axis=0), paddle.concat(preds, axis=0)
